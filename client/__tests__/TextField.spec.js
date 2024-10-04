05792a5b9d5e955cbe454e28fff7515c52ad67bc
Jake Smith <111781011+jakeesmith01@users.noreply.github.com>
2024-10-04 16:10:49 +0000

    TextField tests written

---
+++ client/__tests__/TextField.spec.js
@@ -0,0 +1,44 @@
+import { render, fireEvent } from '@testing-library/vue'
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import TextField from '../src/components/forms/TextField.vue'
+import InputText from 'primevue/inputtext'
+
+describe ('TextField', () => {
+    
+    const props = {
+        field: 'testField',
+        label: 'Test Field',
+        icon: 'pi pi-user',
+        disabled: false,
+        modelValue: '',
+        errors: {
+            testField: [{message:'Field is required'}]
+        }
+    }
+
+    it('renders a text field', () => {
+        const wrapper = mount(TextField, {props})
+
+        expect(wrapper.find('label').text()).toBe(props.label)
+    })
+
+    it('enables input when the disabled prop is false', () => {
+        const wrapper = mount(TextField, { props })
+
+        const input = wrapper.find('input')
+        expect(input.attributes('disabled')).toBeUndefined()
+    })
+
+    it('disables the input when the disabled prop is true', () => {
+        const wrapper = mount(TextField, {
+            props:{
+                ...props,
+                disabled: true
+            }
+        })
+
+        const input = wrapper.find('input')
+        expect(input.attributes('disabled')).exist
+    })
+})

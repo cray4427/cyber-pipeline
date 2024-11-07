5ca500e7ad54e234f42483bca62e803dde4d6406
Patrick Arnold <patricka02@ksu.edu>
2024-11-07 05:12:36 +0000

    District info prepped

--- server/seeds/initial_data.js
+++ server/seeds/initial_data.js
@@ -7,10 +7,51 @@ export async function seed (knex) {
   var then = new Date(now)
   then.setMinutes(then.getMinutes() - 5)
 
-  //#region TESTING DATA -- FIELDS CREATION
 
-    //#region Fake Users
-    const numUsers = 50
+  //#region INITIALIZE DATA -- FIELDS CREATION
+    //#region Users
+    const initialUsers = [
+      { id: 1, eid: "test-admin", name: 'Test Administrator', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { id: 2, eid: "russfeld", name: 'Russell Feldhausen', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { id: 3, eid: "test-student", name: 'Test Student', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { id: 4, eid: "weeser", name: 'Joshua Weese', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { id: 5, eid: "nhbean", name: 'Nathan Bean', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+    ]
+    const firstUsersLength = initialUsers.length+1
+    //#endregion
+  
+    //#region Roles
+    const initialRoles = [
+      { id: 1, name: 'admin', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { id: 2, name: 'user', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' }
+    ]
+    //#endregion
+
+    //#region User Roles
+    const initialUserRoles = [
+      { user_id: 1, role_id: 1, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { user_id: 2, role_id: 1, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { user_id: 3, role_id: 2, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { user_id: 4, role_id: 1, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+      { user_id: 5, role_id: 1, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
+    ]
+    //#endregion
+    //#region Districts
+    const initialDistricts = [
+      { id: 1, usd: '380', name: 'Vermillion', url: 'https://www.usd380.com/', locale: 43, notes: 'Vermillion notes', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin', },
+      { id: 2, usd: '501', name: 'Topeka', url: 'https://www.topekapublicschools.net/', locale: 12, notes: 'Topeka notes', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin', },
+      { id: 3, usd: '233', name: 'Olathe', url: 'https://www.olatheschools.org/', locale: 21, notes: 'Olathe notes', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin', },
+      { id: 4, usd: '259', name: 'Wichita', url: 'https://www.usd259.org/', locale: 11, notes: 'Wichita notes', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin', },
+    ]
+    //#endregion { id: 1, usd: '}
+  //#endregion
+
+
+  //#region ADD TESTING DATA -- FIELDS CREATION
+  const numUsers = 300      // MAX --> 200
+  const numDistricts = 10   // MAX --> 
+
+    //#region Users
 
     const fakeFN = [
       "James", "Mary", "Michael", "Patricia", "Robert", "Jennifer", "John", "Linda", "David", "Elizabeth", "William", "Barbara", 
@@ -25,7 +66,10 @@ export async function seed (knex) {
       "Sara", "Lawrence", "Anne", "Nicholas", "Roy", "Wanda", "Benjamin", "Bonnie", "Samuel", "Julia", "Ruby", "Norma", "Dennis", 
       "Paula", "Diane", "Wayne", "Heather", "Jesse", "Eva", "Alan", "Debbie", "Phillip", "April", "Leslie", "Johnny", "Lillian", 
       "Victor", "Joanne", "Martin", "Emma", "Lori", "Alexander", "Carrie", "Tina", "Martha", "Shirley", "Jerry", "Kathy", "Jeremy", 
-      "Austin", "Pauline", "Chris", "Ethel", "Bryan", "Lorraine", "Lynn", "Loretta", "Marion", "Katie"
+      "Austin", "Pauline", "Chris", "Ethel", "Bryan", "Lorraine", "Lynn", "Loretta", "Marion", "Katie", "Dana", "Maureen", "Geraldine",
+      "Tracy", "Peggy", "Gladys", "Holly", "Jo", "Eleanor", "Janice", "Sherry", "Doris", "Olivia", "Florence", "Jean", "Carla",
+      "Vicki", "Tanya", "Regina", "Charlene", "Vivian", "Sylvia", "Marjorie", "Hilda", "Jill", "Kristin", "Veronica", "Jennie",
+      "Nora", "Margie", "Nina", "Cassandra", "Leah", "Penny", "Kay", "Priscilla", "Naomi", "Carole", "Brandy", "Olga", "Billie",
     ]
 
     const fakeLN = [
@@ -43,68 +87,121 @@ export async function seed (knex) {
       "Grant", "Knight", "Ferguson", "Rose", "Stone", "Hawkins", "Dunn", "Perkins", "Hudson", "Spencer", "Gardner", "Stephens", "Payne",
       "Pierce", "Berry", "Matthews", "Arnold", "Wagner", "Willis", "Ray", "Watkins", "Olson", "Carroll", "Duncan", "Snyder", "Hart",
       "Cunningham", "Bradley", "Lane", "Andrews", "Ruiz", "Harper", "Fox", "Riley", "Armstrong", "Carpenter", "Weaver", "Greene", "Lawrence",
-      "Elliott", "Chavez", "Sims", "Austin", "Peters", "Kelley", "Franklin", "Lawson", "Fields", "Gutierrez", "Ryan", "Schmidt", "Carr"
+      "Elliott", "Chavez", "Sims", "Austin", "Peters", "Kelley", "Franklin", "Lawson", "Fields", "Gutierrez", "Ryan", "Schmidt", "Carr",
+      "Vasquez", "Castillo", "Wheeler", "Chapman", "Oliver", "Montgomery", "Richards", "Williamson", "Johnston"
     ]
 
-    const initialUsers = [
-      { id: 1, eid: "test-admin", name: 'Test Administrator', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
-      { id: 2, eid: "russfeld", name: 'Russell Feldhausen', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
-      { id: 3, eid: "test-student", name: 'Test Student', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
-      { id: 4, eid: "weeser", name: 'Joshua Weese', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
-      { id: 5, eid: "nhbean", name: 'Nathan Bean', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
-    ]
-
-    function getRandomName() {
-      const fN = fakeFN[Math.floor(Math.random() * fakeFN.length)]
-      const lN = fakeLN[Math.floor(Math.random() * fakeLN.length)]
+    function getName(index) {
+      const fN = fakeFN[index] 
+      const lN = fakeLN[index]
       return [fN, lN]
     }
 
-    for (let i = 6; i <= numUsers; i++) {
-      const local_name = getRandomName()
+    for (let i=0; i < Math.min(numUsers, fakeFN.length, fakeLN.length); i++) {
+      const local_name = getName(i)
       const local_eid = ['fake', local_name[1], i.toString()].join('-')
-      initialUsers.push({ id: i, eid: local_eid, name: `${local_name.join(' ')}`, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' })
+      initialUsers.push({ id: i+firstUsersLength, eid: local_eid, name: `${local_name.join(' ')}`, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' })
     }
     //#endregion
-    //#region Fake Districts
-  
-    //#endregion
-  //#endregion
-
-  //#region REAL DATA -- FIELDS CREATION
-    //#region Roles
-    const initialRoles = [
-      { id: 1, name: 'admin', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' },
-      { id: 2, name: 'user', created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin' }
+    //#region Districts
+    const usdNames = [
+      "Erie-Galesburg", "Cimarron-Ensign", "Cheylin", "Rawlins County", "Western Plains", "Hoxie", 
+      "Rock Hills", "Washington County Schools", "Republic County", "Thunder Ridge Schools", 
+      "Doniphan West Schools", "Central Plains", "Prairie Hills", "Riverside", "Nemaha Central",
+      "Greeley County Schools", "Turner-Kansas City", "Piper-Kansas City", "Bonner Springs",
+      "Bluestem", "Remington-Whitewater", "Fort Leavenworth", "Trego", "Moscow Public Schools",
+      "Hugoton Public Schools", "Norton Community Schools", "Northern Valley", "Ulysses", "Lakin",
+      "Deerfield", "Rolla", "Elkhart", "Minneola", "Ashland", "Barnes", "Clifton-Clyde", "Fowler",
+      "Meade", "Unified School District 227", "Blue Valley", "Spring Hill", "Gardner Edgerton",
+      "De Soto", "Olathe", "Fort Scott", "Uniontown", "Smith Center", "North Ottawa County",
+      "Twin Valley", "Wallace County Schools", "Weskan",
+      "Lebo-Waverly", "Burlington", "LeRoy-Gridley", "Northeast", "Cherokee", "Girard", 
+      "Frontenac Public Schools", "Pittsburg", "North Lyon County", "Southern Lyon County", 
+      "Emporia", "Barber County North", "South Barber", "Marmaton Valley USD 256", "Iola", 
+      "Humboldt", "Wichita", "Derby", "Haysville", "Valley Center Public Schools", "Mulvane", 
+      "Clearwater", "Goddard", "Maize", "Renwick", "Cheney", "Palco", "Plainville", "Stockton", 
+      "Waconda", "Beloit", "Oakley", "Triplains", "Graham County", "West Elk", "Elk Valley", 
+      "Chase County", "Cedar Vale", "Chautauqua County Community", "West Franklin", 
+      "Central Heights", "Wellsville", "Ottawa",
+      "Grinnell Public Schools", "Wheatland", "Quinter Public Schools", "Oberlin", 
+      "St. Francis Community Schools", "Lincoln", "Sylvan Grove", "Comanche County", 
+      "Ness City", "Salina", "Southeast of Saline", "Ell-Saline", "Hutchinson Public Schools", 
+      "Nickerson", "Fairfield", "Pretty Prairie", "Haven Public Schools", "Buhler", 
+      "Brewster", "Colby Public Schools", "Golden Plains", "Wamego", "Kaw Valley", 
+      "Onaga-Havensville-Wheaton", "Rock Creek", "Phillipsburg", "Logan", "Ellsworth", 
+      "Wabaunsee", "Mission Valley", "Kingman-Norwich", "Cunningham", "Concordia", 
+      "Southern Cloud", "North Jackson", "Holton", "Royal Valley", "Valley Falls",
+      "Jefferson County North", "Jefferson West", "Oskaloosa Public Schools", "McLouth", 
+      "Perry Public Schools", "Pleasanton", "Seaman", "Jayhawk", "Kinsley-Offerle", 
+      "Baldwin City", "Stafford", "St John-Hudson", "Macksville", "Goodland", "Wellington", 
+      "Ellinwood Public Schools", "Conway Springs", "Belle Plaine", "Oxford", 
+      "Argonia Public Schools", "Caldwell", "Chaparral Schools", "Prairie View", 
+      "Holcomb", "Marysville", "Garnett", "Woodson", "Osawatomie", "Unified School District 368", 
+      "Burrton", "Montezuma", "Silver Lake", "Newton", "Sublette", "Circle", "Sterling", 
+      "Atchison County", "Riley County", "Clay County", "Vermillion", "Spearville",
+      "Pratt", "Manhattan-Ogden", "Blue Valley", "Andover", "Madison-Virgil", "Altoona-Midway", 
+      "Ellis", "Eureka", "Hamilton", "Osborne County", "Solomon", "Rose Hill Public Schools", 
+      "LaCrosse", "Douglass Public Schools", "Centre", "Peabody-Burns", "Paradise", 
+      "Smoky Valley", "Chase-Raymond", "Augusta", "Otis-Bison", "Riverton", "Lyons", 
+      "Russell County", "Marion-Florence", "Atchison Public Schools", "Durham-Hillsboro-Lehigh", 
+      "Goessel", "Hoxie Community Schools", "Chanute Public Schools", "Hiawatha", "Louisburg",
+      "Morris County", "McPherson", "Canton-Galva", "Osage City", "Lyndon", "Kiowa County", 
+      "Moundridge", "Pike Valley", "Great Bend", "Troy Public Schools", "South Brown County", 
+      "Hoisington", "Victoria", "Santa Fe Trail", "Abilene", "Caney Valley", "Auburn-Washburn", 
+      "Skyline Schools", "Sedgwick Public Schools", "Halstead", "Dodge City", "Little River", 
+      "Coffeyville", "Independence", "Cherryvale", "Inman", "Easton", "Shawnee Heights", 
+      "Stanton County", "Leavenworth", "Burlingame Public School",
+      "Marais Des Cygnes Valley", "Garden City", "Basehor-Linwood", "Bucklin", "Hesston", 
+      "Neodesha", "Central", "Udall", "Tonganoxie", "Winfield", "Scott County", "Leoti", 
+      "Healy Public Schools", "Lansing", "Arkansas City", "Dexter", "Chapman", "Haviland", 
+      "Geary County Schools", "Copeland", "Ingalls", "Crest", "Liberal", "Rural Vista", 
+      "Dighton", "Kismet-Plains", "Fredonia", "Herington", "Hays", "El Dorado", "Eudora", 
+      "Flinthills", "Columbus", "Syracuse", "Fort Larned", "Pawnee Heights", "Lawrence", 
+      "Valley Heights", "Galena", "Kansas City", "Topeka Public Schools", "Lewis", "Parsons", 
+      "Oswego", "Chetopa-St. Paul", "Labette County", "Satanta", "Baxter Springs", 
+      "South Haven", "Attica", "Shawnee Mission"
     ]
+
+    const usdNumbers = [
+      "101", "102", "103", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114",
+      "115", "200", "202", "203", "204", "205", "206", "207", "208", "209", "210", "211", "212",
+      "214", "215", "216", "217", "218", "219", "220", "223", "224", "225", "226", "227", "229",
+      "230", "231", "232", "233", "234", "235", "237", "239", "240", "241", "242", "243", "244",
+      "245", "246", "247", "248", "249", "250", "251", "252", "253", "254", "255", "256", "257",
+      "258", "259", "260", "261", "262", "263", "264", "265", "266", "267", "268", "269", "270",
+      "271", "272", "273", "274", "275", "281", "282", "283", "284", "285", "286", "287", "288",
+      "289", "290", "291", "292", "293", "294", "297", "298", "299", "300", "303", "305", "306",
+      "307", "308", "309", "310", "311", "312", "313", "314", "315", "316", "320", "321", "322",
+      "323", "325", "326", "327", "329", "330", "331", "332", "333", "334", "335", "336", "337",
+      "338", "339", "340", "341", "342", "343", "344", "345", "346", "347", "348", "349", "350",
+      "351", "352", "353", "355", "356", "357", "358", "359", "360", "361", "362", "363", "364",
+      "365", "366", "367", "368", "369", "371", "372", "373", "374", "375", "376", "377", "378",
+      "379", "380", "381", "382", "383", "384", "385", "386", "387", "388", "389", "390", "392",
+      "393", "394", "395", "396", "397", "398", "399", "400", "401", "402", "403", "404", "405",
+      "407", "408", "409", "410", "411", "412", "413", "415", "416", "417", "418", "419", "420",
+      "421", "422", "423", "426", "428", "429", "430", "431", "432", "434", "435", "436", "437",
+      "438", "439", "440", "443", "444", "445", "446", "447", "448", "449", "450", "452", "453",
+      "454", "456", "457", "458", "459", "460", "461", "462", "463", "464", "465", "466", "467",
+      "468", "469", "470", "471", "473", "474", "475", "476", "477", "479", "480", "481", "482",
+      "483", "484", "487", "489", "490", "491", "492", "493", "494", "495", "496", "497", "498",
+      "499", "500", "501", "502", "503", "504", "505", "506", "507", "508", "509", "511", "512"
+    ]
+
+    for (let i=0; i < Math.min(numDistricts, usdNames.length, usdNumbers.length); i++) {
+      const local_name = usdNames[i]
+      const local_usd = usdNumbers[i]
+      initialDistricts.push({ id: i+1, usd: local_usd, name: local_name, url: `https://${local_name.replace(' ', '').toLowerCase()}.org/`, 
+        locale: i+1, notes: `${local_name} notes`, created_at: now, updated_at: now, created_by: 'test-admin', updated_by: 'test-admin', })
+    }
     //#endregion
   //#endregion
 
   // Users
   await knex('users').del()
   await knex('users').insert(initialUsers)
-
   // Roles
   await knex('roles').del()
-  await knex('roles').insert([
-    {
-      id: 1,
-      name: 'admin',
-      created_at: now,
-      updated_at: now,
-      created_by: 'test-admin',
-      updated_by: 'test-admin',
-    },
-    {
-      id: 2,
-      name: 'user',
-      created_at: now,
-      updated_at: now,
-      created_by: 'test-admin',
-      updated_by: 'test-admin',
-    },
-  ])
-
+  await knex('roles').insert(initialRoles)
   // User Roles
   await knex('user_roles').del()
   await knex('user_roles').insert([
@@ -133,61 +230,9 @@ export async function seed (knex) {
       updated_by: 'test-admin',
     }, 
   ])
-
   // Districts
   await knex('districts').del()
-  await knex('districts').insert([
-    {
-      id: 1,
-      usd: '380',
-      name: 'Vermillion',
-      url: 'https://www.usd380.com/',
-      locale: 43,
-      notes: 'Vermillion notes',
-      created_at: now,
-      updated_at: now,
-      created_by: 'test-admin',
-      updated_by: 'test-admin',
-    },
-    {
-      id: 2,
-      usd: '501',
-      name: 'Topeka',
-      url: 'https://www.topekapublicschools.net/',
-      locale: 12,
-      notes: 'Topeka notes',
-      created_at: now,
-      updated_at: now,
-      created_by: 'test-admin',
-      updated_by: 'test-admin',
-    },
-    {
-      id: 3,
-      usd: '233',
-      name: 'Olathe',
-      url: 'https://www.olatheschools.org/',
-      locale: 21,
-      notes: 'Olathe notes',
-      created_at: now,
-      updated_at: now,
-      created_by: 'test-admin',
-      updated_by: 'test-admin',
-    },
-    {
-      id: 4,
-      usd: '259',
-      name: 'Wichita',
-      url: 'https://www.usd259.org/',
-      locale: 11,
-      notes: 'Wichita notes',
-      created_at: now,
-      updated_at: now,
-      created_by: 'test-admin',
-      updated_by: 'test-admin',
-    },
-  ])
-  
-
+  await knex('districts').insert(initialDistricts)
   // Teachers
   await knex('teachers').del()
   await knex('teachers').insert([
